9df21cee4e9d55b227a80426b711f99d1766ba7b
izzydotexe <israel.aristide@dawsoncollege.qc.ca>
2024-02-28 14:07:34 -0500

    moderation module scaffolding

---
+++ arc3/Core/Modules/ModerationModule.cs
@@ -0,0 +1,12 @@
+
+using Discord.Interactions;
+
+namespace Arc3.Core.Modules;
+
+
+internal class ModerationModule // : InteractionModuleBase<SocketInteractionContext>
+{
+
+  public InteractionService InteractionService { get; set; }
+
+}
