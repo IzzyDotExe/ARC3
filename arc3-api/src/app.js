9347caf937519f2763960da896494d885f4002cc
izzydotexe <israel.aristide@dawsoncollege.qc.ca>
2024-03-16 22:38:17 -0400

    fix react app serving

--- arc3-api/src/app.js
+++ arc3-api/src/app.js
@@ -2,8 +2,15 @@ const express = require('express');
 const v1 = require('./v1/v1.js');
 
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
 app.use('/api', v1);
 
-app.use(express.static('./build'));
+app.use('/static', express.static(process.env.BUILD_PATH+'/static'?? './build/static'))
+
+app.get('/*', (req, res) => {
+  res.sendFile('index.html', { root: process.env.BUILD_PATH?? "./build" });
+});
 
 module.exports = app;
