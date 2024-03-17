0ee9897861c4b3442ef1695fa2197ec3a195b22b
izzydotexe <israel.aristide@dawsoncollege.qc.ca>
2024-03-16 23:32:22 -0400

    rename 2

--- arc3-api/src/v1/routers/TranscriptRoutes.js
+++ arc3-api/src/v1/routers/TranscriptRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-const { GetTranscripts, GetMailIds } = require('../controllers/TranscriptController.js');
+const { GetTranscripts, GetMailIds } = require('../controllers/TranscriptControllers.js');
 
 // Send the user all the transcripts
 router.get('/:mailId', GetTranscripts);
