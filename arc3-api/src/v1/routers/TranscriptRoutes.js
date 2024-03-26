32473f6a0e13e7576be6a87867704e11f7ae870e
izzydotexe <israel.aristide@dawsoncollege.qc.ca>
2024-03-26 15:29:30 -0400

    ammend

--- arc3-api/src/v1/routers/TranscriptRoutes.js
+++ arc3-api/src/v1/routers/TranscriptRoutes.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const router = express.Router();
 
+const authenticated = require('../../auth/middlewares/authenticated');
+
 const { GetTranscripts, GetMailIds } = require('../controllers/TranscriptControllers.js');
 
 // Send the user all the transcripts
-router.get('/:mailId', GetTranscripts);
-router.get('/', GetMailIds);
+router.get('/:mailId', authenticated, GetTranscripts);
+router.get('/', authenticated, GetMailIds);
 
 module.exports = router;
