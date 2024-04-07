2209ecd37b8bff627aec4b92447c5d5d2c6193fd
izzydotexe <israel.aristide@dawsoncollege.qc.ca>
2024-04-07 14:06:25 -0400

    remove appeal routes temporarily

--- arc3-dash/src/components/Navbar.jsx
+++ arc3-dash/src/components/Navbar.jsx
@@ -6,8 +6,8 @@ export default function Navbar() {
       <h2>Arc V3</h2>
       <ul>
         <li><a href="/">Home</a></li>
-        <li><a href='/appeal'>Appeal</a></li>
-        <li><a href='/appeals'>Appeals</a></li>
+        {/* <li><a href='/appeal'>Appeal</a></li>
+        <li><a href='/appeals'>Appeals</a></li> */}
         <li><a href="/transcripts/">Transcripts</a></li>
       </ul>
     </nav>
