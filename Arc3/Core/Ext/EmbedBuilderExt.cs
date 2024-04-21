cd0915ead1acbe14fea8fe283279cf6b69190b54
izzydotexe <israel.aristide@dawsoncollege.qc.ca>
2024-04-20 23:29:03 -0400

    modify modmail style to customize footer

--- Arc3/Core/Ext/EmbedBuilderExt.cs
+++ Arc3/Core/Ext/EmbedBuilderExt.cs
@@ -5,11 +5,11 @@ namespace Arc3.Core.Ext;
 
 public static class EmbedBuilderExt
 {
-    public static EmbedBuilder WithModMailStyle(this EmbedBuilder builder, DiscordSocketClient clientInstance)
+    public static EmbedBuilder WithModMailStyle(this EmbedBuilder builder, DiscordSocketClient clientInstance, string footer = "Modmail")
     {
         return builder
             .WithColor(Color.DarkRed)
             .WithTimestamp(DateTimeOffset.UtcNow)
-            .WithFooter($"ARC v{Arc3.ArcVersion} - Modmail", clientInstance.CurrentUser.GetAvatarUrl());
+            .WithFooter($"ARC v{Arc3.ArcVersion} - {footer}", clientInstance.CurrentUser.GetAvatarUrl());
     }
 }
