ce180bd8fb9f2faad49b8e88ce4ee03bda222e88
izzydotexe <israel.aristide@dawsoncollege.qc.ca>
2024-04-22 18:00:28 -0400

    Add re-adding jail role apon rejoin.

--- Arc3/Core/Services/JailService.cs
+++ Arc3/Core/Services/JailService.cs
@@ -2,6 +2,7 @@
 
 using Arc3.Core.Schema;
 using Arc3.Core.Schema.Ext;
+using Discord;
 using Discord.Interactions;
 using Discord.WebSocket;
 
@@ -15,7 +16,47 @@ public class JailService : ArcService {
     : base(clientInstance, interactionService, "JAIL") {
       _dbService = dbService;
       clientInstance.MessageReceived += ClientInstanceOnMessageReceived;
+      clientInstance.UserJoined += ClientInstanceOnUserJoined;
     }
+
+  private async Task ClientInstanceOnUserJoined(SocketGuildUser arg)
+  {
+    
+    // Get all the jails
+    var jails = await _dbService.GetJailsAsync();
+
+    // If the user that joined has a jail logged. 
+    if (jails.Any(x => x.UserSnowflake == (long)arg.Id))
+    {
+      
+      var jail = jails.First(x => x.UserSnowflake == (long)arg.Id);
+      
+      // give them the jailed role and make sure the channel has allowed them.
+      
+      // Get some needed vars 
+      var channel = await jail.GetChannel(_clientInstance);
+      var user = arg;
+      var guildConfig = _dbService.Config[user.Guild.Id];
+      var guild = arg.Guild;
+      
+      // Set the channel perms
+      var perms = new OverwritePermissions(viewChannel: PermValue.Allow);
+      await channel.AddPermissionOverwriteAsync(user, perms);
+      
+      // Give the role
+      try {
+        var jailedRoleSnowflake = ulong.Parse(guildConfig["jailedrole"]);
+        var jailRole = guild.GetRole(jailedRoleSnowflake);
+        await user.AddRoleAsync(jailRole);
+        // await channel.SendMessageAsync("User rejoined! Adding jail role.");
+      } catch (Exception e) {
+        await channel.SendMessageAsync("Jail role not found or failed to give role to user!");
+      }
+
+    }
+
+    
+  }
 
   private async Task ClientInstanceOnMessageReceived(SocketMessage arg)
   {
