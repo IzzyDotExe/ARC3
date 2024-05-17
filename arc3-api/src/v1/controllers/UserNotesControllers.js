e8191a142a1246d2e6d4b4e883e5213b5ea01edc
izzydotexe <israel.aristide@dawsoncollege.qc.ca>
2024-05-16 22:46:51 -0400

    implement notes controllers

--- arc3-api/src/v1/controllers/UserNotesControllers.js
+++ arc3-api/src/v1/controllers/UserNotesControllers.js
@@ -13,8 +13,8 @@ async function GetUserNotes(req, res) {
 
   try {
 
-    const notes = await UserNote.find({ 'guildsnowflake': guildid, 'usersnowflake': userid }).sort({ 'date': 1 });
-    res.status(200).json(notes);
+    const notes = await UserNote.find();
+    res.status(200).json(notes.filter(x => x.guildsnowflake == guildid && x.usersnowflake == userid));
 
   } catch (e) {
     console.error(e.message)
@@ -30,9 +30,59 @@ async function GetUserNotes(req, res) {
 }
 
 async function GetUserNotesBy(req, res) {
+  
   const guildid = req.params.guildid;
+  const userid = req.params.userid;
+
+  if (guildid === undefined || userid === undefined) {
+    res.status(400);
+    res.json({'status': 404, 'error': 'could not find those usernotes'});
+    return;
+  }
+
+  try { 
+
+    const notes = await UserNote.find();
+    res.status(200).json(notes.filter(x => x.guildsnowflake == guildid ))
+
+
+  } catch (e) {
+    
+    console.error(e.message);
+
+    res.status(500);
+    res.json({
+      'status': 500,
+      'error': 'An Error occured. Please try again later.'
+    });  
+
+  }
+
+  
 }
 
 async function GetUsers(req, res) {
   const guildid = req.params.guildid;
+  if (guildid === undefined) {
+    res.status(400);
+    res.json({'status': 404, 'error': 'could not find those usernotes'});
+    return;
+  }
+
+  try {
+    let userids = await UserNote.find()
+    userids = [... new Set(userids.filter(x => x.guildsnowflake == guildid).map(x => x.usersnowflake))]
+    res.status(200).json(userids)
+
+  } catch (e) {
+    console.error(e.message);
+
+    res.status(500);
+    res.json({
+      'status': 500,
+      'error': 'An Error occured. Please try again later.'
+    });
+  }
 }
+
+module.exports = {GetUsers, GetUserNotes, GetUserNotesBy}
