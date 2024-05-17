ab9e059af8dc57a47ce2815d699b8043356d7b46
izzydotexe <israel.aristide@dawsoncollege.qc.ca>
2024-05-17 15:07:42 -0400

    add notes page

---
+++ arc3-dash/src/routes/Notes.jsx
@@ -0,0 +1,51 @@
+import Navbar from '../components/Navbar';
+import { useState, useEffect} from 'react';
+import axios from 'axios';
+import { useParams } from 'react-router';
+import { Outlet } from "react-router-dom";
+
+import './Notes.css'
+
+import {TranscriptMemberComponent} from '../components/TranscriptMemberComponent'
+
+export default function Notes() {
+
+  const {guild} = useParams();
+  const [notesUsers, setNoteUsers] = useState([]);
+
+  useEffect(() => {
+    axios.get(`/api/notes/${guild}`).then(res => {
+      setNoteUsers(res.data);
+    })
+  }, [guild])
+
+  return (
+
+    <>
+    
+    <div className="App">
+
+      <title>User Notes</title>
+
+      <Navbar/>
+
+      <main>
+        <div className="NotesPick">
+          <div className = "sidebar">
+            {notesUsers.map(x => <a href={`/${guild}/notes/${x}`}><TranscriptMemberComponent userid={x}/></a>)}
+          </div>
+          
+          <div className = "notes-sec">
+            <Outlet/>
+          </div>
+          
+        </div>
+      </main>
+
+    </div>
+    
+    </>
+
+  )
+
+}
