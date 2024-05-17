2dc8c5b0e5a07ca2fce5461511b4a75693709eb5
izzydotexe <israel.aristide@dawsoncollege.qc.ca>
2024-05-16 22:46:38 -0400

    add notes routes

--- arc3-api/src/v1/routers/UsernoteRoutes.js
+++ arc3-api/src/v1/routers/UsernoteRoutes.js
@@ -3,9 +3,11 @@ const router = express.Router();
 
 const authenticated = require('../../auth/middlewares/authenticated.js');
 
-const { GetUserNotes, GetUserNotesBy, GetUsers } = require('../controllers/UserNotesControllers.js') 
+const { GetUsers, GetUserNotes, GetUserNotesBy } = require('../controllers/UserNotesControllers.js') 
 
 
 router.get('/:guildid/:userid', authenticated, GetUserNotes);
 router.get('/:guildid', authenticated, GetUsers);
-router.get('/:guildid/:auhorid', authenticated, GetUserNotesBy);
+router.get('/:guildid/by/:userid', authenticated, GetUserNotesBy);
+
+module.exports = router;
