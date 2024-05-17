f9e17426ec0faa58f76528e89739ce9931b0efa6
izzydotexe <israel.aristide@dawsoncollege.qc.ca>
2024-05-17 15:07:49 -0400

    user notes list page

---
+++ arc3-dash/src/routes/UserNotes.jsx
@@ -0,0 +1,26 @@
+import { useParams } from 'react-router';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
+import {Note} from '../components/Note'
+
+
+export default function UserNotes() {
+
+  const { userid, guild } = useParams();
+
+  const [usernotes, setUserNotes] = useState([])
+
+  useEffect(() => {
+    axios.get(`/api/notes/${guild}/${userid}`).then(res => {
+      setUserNotes(res.data);
+    })
+  }, [guild, userid])
+
+  return (
+    <>
+    {usernotes.map(x => <Note data={x}/>)}
+    </>
+  )
+
+}
