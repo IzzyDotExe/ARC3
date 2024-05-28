fcd9519f7a680e85090ba3c8eeef87c5cbf2e351
izzydotexe <israel.aristide@dawsoncollege.qc.ca>
2024-05-28 09:34:10 -0400

    backp db process

--- arc3-tasks/backup-db.js
+++ arc3-tasks/backup-db.js
@@ -1,5 +1,69 @@
-// Get all  collections from the db 
+// Connect to the mongo database
+const { MongoClient } =  require("mongodb");
+const tar = require('tar');
+const fs = require('fs');
+const debug = process.env.DEBUG;
 
-// Save them in files and encrypt them
+const uri = process.env.MONGODB_URI;
+var it = [ ... process.argv ] 
 
-// Output somewhere useful
+if (debug === 'true')
+  console.log(it)
+
+it.shift()
+it.shift()
+
+if (debug === 'true')
+  console.log(it)
+
+const client = new MongoClient(uri);
+
+async function run(collections) {
+
+    const db = client.db('Arc3');
+    const files = []
+
+    // Go through all the tables that have data that is temp (Modmails, Jails, Appeals, Karaoke)
+    collections.forEach(async x => {
+      const collection = db.collection(x);
+
+      const data = await collection.find();
+      const items = []
+      for await ( let item of data) {
+        
+        if (debug === 'true')
+          console.log(item)
+
+        items.push(item)
+      }
+
+      const json = JSON.stringify(items);
+
+      fs.writeFile(`./bkup/${x}.json`, json, 'utf8', () => {
+      })      
+
+    })
+
+}
+
+client.connect().then(x => {
+  
+  run(it).then(x => {
+    setTimeout(async () => {
+      await client.close()
+
+      tar.create(
+        { file: `./out/backup-${Date.now()}.tar.gz`, gzip: true },
+        ['./bkup']
+      ).then(_ => {
+
+      })
+
+    }, 5000);
+    
+  }).catch(console.dir)
+    
+});
+
+
+
