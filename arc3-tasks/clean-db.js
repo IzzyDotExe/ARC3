a6908d488d415fee81cc3c332928736f67eae35a
izzydotexe <israel.aristide@dawsoncollege.qc.ca>
2024-05-28 09:34:17 -0400

    clean db process

--- arc3-tasks/clean-db.js
+++ arc3-tasks/clean-db.js
@@ -1,15 +1,22 @@
 // Connect to the mongo database
 const { MongoClient } =  require("mongodb");
+const debug = process.env.DEBUG;
 
-const uri = process.env.MONGODB_URI
-console.log(uri)
+const uri = process.env.MONGODB_URI;
+var it = [ ... process.argv ] 
+
+if (debug === 'true')
+  console.log(it)
+
+it.shift()
+it.shift()
+
+if (debug === 'true')
+  console.log(it)
 
 const client = new MongoClient(uri);
 
 async function run(collections) {
-
-  try {
-    await client.connect();
 
     const db = client.db('Arc3');
 
@@ -22,13 +29,19 @@ async function run(collections) {
 
     })
 
-  } finally {
-    await client.close()
-  }
-
 }
 
-run(["jails"]).catch(console.dir)
+client.connect().then(x => {
+  
+  run(it).then(x => {
+
+    setTimeout(async () => {
+      await client.close()
+    }, 3000)
+    
+  }).catch(console.dir)
+    
+});
 
 
 
