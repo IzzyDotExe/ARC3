717f5346ef0c71fe3137b6da84b306c6276219e3
izzydotexe <israel.aristide@dawsoncollege.qc.ca>
2024-05-28 09:34:35 -0400

    data compliance process

--- arc3-tasks/data-compliance.js
+++ arc3-tasks/data-compliance.js
@@ -1,3 +1,30 @@
 // connect to the databse 
+const { MongoClient } =  require("mongodb");
+
+const uri = process.env.MONGODB_URI
+const client = new MongoClient(uri);
 
 // remove all transcripts older than 30 days
+
+async function run(collections) {
+
+  const db = client.db('Arc3');
+
+  const collection = db.collection("transcripts");
+
+  // Delete all docs older than 30 days
+  collection.deleteMany( { createdat : {"$lt" : new Date(Date.now() - 30*24*60*60 * 1000) } })
+
+}
+
+client.connect().then(x => {
+  
+  run().then(x => {
+
+    setTimeout(async () => {
+      await client.close()
+    }, 3000)
+    
+  }).catch(console.dir)
+    
+});
