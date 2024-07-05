a6d8030faafb26d11ada3e066b52dc6033624814
izzydotexe <israel.aristide@dawsoncollege.qc.ca>
2024-07-05 14:31:12 -0400

    fix guild component

--- arc3-dash/src/components/Guild.jsx
+++ arc3-dash/src/components/Guild.jsx
@@ -1,14 +1,25 @@
+import "./Guild.css"
 
+export default function Guild({guild, key, href}) {
 
-export default function Guild({guild, key}) {
+  const GuildIcon = guild && guild.icon? `https://cdn.discordapp.com/icons/${guild.id}/${guild.icon}.png?size=128` : "/missing.png"; 
 
-  const GuildIcon = guild && guild.icon? `https://cdn.discordapp.com/icons/${guild.id}/${guild.icon}.png?size=128` : "/missing.jpg"; 
   return (
     <div className="guild">
-      
-      <img src={GuildIcon} alt="" />
-      <p>{guild.name}</p>
+      {href && <a draggable="false" href={href}>
+        <IconLabel tag={guild.name} src={GuildIcon}/>
+      </a>}
 
+      {!href && <>
+        <IconLabel tag={guild.name} src={GuildIcon}/>
+      </>}
     </div>
   )
+}
+
+export function IconLabel({tag, src}) {
+  return (<>
+    <img draggable="false" src={src} alt="" />
+    <p>{tag}</p>
+  </>)
 }
