3744369d6a3c61f3998cbe2986a7046b3773210d
izzydotexe <israel.aristide@dawsoncollege.qc.ca>
2024-07-06 22:20:46 -0400

    fixes

--- arc3-dash/src/components/Insights/Insight.jsx
+++ arc3-dash/src/components/Insights/Insight.jsx
@@ -1,14 +1,19 @@
 import InfoBox from '../Util/Infobox';
 import './Insight.css'
+import { timeAgo } from "../../lib/utils";
+
+
 export default function Insight({ children, insight }) {
-  return (  
-    <div className="insight">
-      <InfoBox inner={true}>
-        <h3>{insight.tagline}</h3>
-        <div className="content">
-          {children}
-        </div>
-      </InfoBox>
-    </div>      
+  return (
+      <div className="insight">
+          <p className="timestamp">{timeAgo(insight.date * 1000)}</p>
+          <InfoBox inner={true}>
+
+              <h3>{insight.tagline}</h3>
+              <div className="content">
+                  {children}
+              </div>
+          </InfoBox>
+      </div>
   )
 }
